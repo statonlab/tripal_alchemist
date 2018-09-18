7ffd7eab5e14e0ebcba0b9db16f72d32ac1d2bf6
bradford.condon <bradford.condon@gmail.com>
2018-09-18 17:21:04 -0400

    passing test if tripal core is modified

--- tests/tripal_alchemist_APITest.php
+++ tests/tripal_alchemist_APITest.php
@@ -5,6 +5,8 @@ namespace Tests;
 use StatonLab\TripalTestSuite\DBTransaction;
 use StatonLab\TripalTestSuite\TripalTestCase;
 
+
+
 class tripal_alchemist_APITest extends TripalTestCase {
 
   use DBTransaction;
@@ -24,9 +26,11 @@ class tripal_alchemist_APITest extends TripalTestCase {
 
   }
 
-  public function test_alchemist_build_transaction_table() {
-
-    //by default, we have genes and mRNA.  let's convert from one to the other.
+  /**
+   * Creates an mRNA and a gene, and publish.  Then update the type_id of the
+   * gene so that it is mRNA, qualifying it for alchemist automatic conversion.
+   */
+  private function create_automatic_entities() {
 
     $gene_term = chado_get_cvterm(['id' => 'SO:0000704']);
     $mrna_term = chado_get_cvterm(['id' => 'SO:0000234']);
@@ -35,17 +39,31 @@ class tripal_alchemist_APITest extends TripalTestCase {
     $mrna = factory('chado.feature')->create(['type_id' => $mrna_term->cvterm_id]);
     $this->publish('feature');
 
-    $mrna_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000234']);
     $gene_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000704']);
-
+    $mrna_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000234']);
 
     $match = ['feature_id' => $gene->feature_id];
     $uvalues = ['type_id' => $mrna_term->cvterm_id];
 
     $updated = chado_update_record('feature', $match, $uvalues);
 
-    //    tripal_alchemist_run_conversion_job();
+    $args = [
+      'gene' => $gene,
+      'mrna' => $mrna,
+    ];
 
+    return $args;
+  }
+
+  /**
+   * @group api
+   * @group form
+   */
+  public function test_alchemist_build_transaction_table() {
+
+    $this->create_automatic_entities();
+    $gene_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000704']);
+    $mrna_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000234']);
 
     $table = tripal_alchemist_build_transaction_table($gene_bundle, $mrna_bundle);
 
@@ -58,5 +76,103 @@ class tripal_alchemist_APITest extends TripalTestCase {
     $this->assertNotEmpty($table['rows']);
   }
 
+
+  public function test_tripal_alchemist_convert_all() {
+
+    $args = $this->create_automatic_entities();
+
+    $gene = $args['gene'];//type: 496
+    $mrna = $args['mrna'];//type: 422
+    $gene_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000704']);
+    $mrna_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000234']);
+
+
+    $source = $gene_bundle;
+    $destination = $mrna_bundle;
+    $trigger = FALSE;
+
+    tripal_alchemist_convert_all($source, $destination, $trigger);
+
+    $result = db_select('chado.feature', 'f')
+      ->fields('f', ['type_id'])
+      ->condition('feature_id', $gene->feature_id)
+      ->execute()
+      ->fetchObject();
+
+    $this->assertNotFalse($result);
+    //Confirm the type_id changed in Chado.
+    $this->assertEquals($mrna->type_id, $result->type_id);
+
+    //TODO: Confirm the entity moved.
+  }
+
+
+  /**
+   *
+   * @group wip
+   */
+  public function test_tripal_alchemist_convert_select_entities_collection() {
+
+    $args = $this->create_automatic_entities();
+
+    $gene = $args['gene'];//type: 496
+    $mrna = $args['mrna'];//type: 422
+    $gene_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000704']);
+    $mrna_bundle = tripal_load_bundle_entity(['accession' => 'SO:0000234']);
+
+
+    //get entity_id of the gene to convert
+
+    $eid = chado_get_record_entity_by_table('feature', $gene->feature_id);
+
+
+    $this->actingAs(1);
+
+    global $user;
+
+    $fields = field_info_instances('TripalEntity', $gene_bundle->name);
+
+    $field_ids = [];
+
+    foreach ($fields as $field) {
+      $field_ids[] = $field['id'];
+    }
+    $collection_details = [
+      'uid' => $user->uid,
+      'collection_name' => 'alchemist_test_collection_PHPUNIT',
+      'bundle_name' => $gene_bundle->name,
+      'ids' => [$eid],
+      'description' => NULL,
+      'fields' => $field_ids,
+    ];
+
+
+
+    module_load_include('tripal', 'inc', 'includes/TripalFieldDownloaders/TripalTabDownloader');
+
+
+    $tec = tripal_create_collection($collection_details);
+
+    $source = $gene_bundle;
+    $destination = $mrna_bundle;
+    $source_entities = [];
+  //  $collection = $tec_id;
+
+
+    tripal_alchemist_convert_select_entities($source, $destination, $source_entities, $tec);
+
+
+    $result = db_select('chado.feature', 'f')
+      ->fields('f', ['type_id'])
+      ->condition('feature_id', $gene->feature_id)
+      ->execute()
+      ->fetchObject();
+
+    $this->assertNotFalse($result);
+    //Confirm the type_id changed in Chado.
+    $this->assertEquals($mrna->type_id, $result->type_id);
+
+    //TODO: confirm entity moved
+  }
 
 }
